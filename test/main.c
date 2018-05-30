11e2a5ff66e93eeede6a11911ff897f3f156544b
Kamal Heib <kamalheib1@gmail.com>
2018-05-30 13:55:54 +0300

    libenet: Passing pci device as argument to main test
    Pass the pci device (d,b,d,f) as arguments to the main test.
    
    Signed-off-by: Kamal Heib <kamalheib1@gmail.com>

--- test/main.c
+++ test/main.c
@@ -16,6 +16,7 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include "enet.h"
 
@@ -24,7 +25,11 @@ int main(int argc, char **argv)
 	struct enet_dev *dev;
 
 	puts("Start testing libenet...");
-	dev = enet_open_dev(0, 1, 0, 0);
+	dev = enet_open_dev(atoi(argv[1]), atoi(argv[2]),
+			    atoi(argv[3]), atoi(argv[4]));
+	if (!dev) {
+		return 1;
+	}
 	enet_close_dev(dev);
 	puts("End testing libenet...");
 	return 0;
