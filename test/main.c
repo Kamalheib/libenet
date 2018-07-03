36cc82129d6b3d20ee707072fc22871dc3c73209
Kamal Heib <kamalheib1@gmail.com>
2018-07-03 11:33:39 +0300

    libenet: Test read/write addr
    Signed-off-by: Kamal Heib <kamalheib1@gmail.com>

--- test/main.c
+++ test/main.c
@@ -19,62 +19,106 @@
 #include <stdlib.h>
 #include <malloc.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <time.h>
 #include "enet.h"
 
-#define TRANSFER_SIZE (8 * 1024)
-#define RUNS 5000
+static int read_indirect(struct enet_dev *dev, uint32_t addr, uint32_t *val)
+{
+	uint32_t write_val;
+	uint32_t high;
+	uint32_t low;
+
+	write_val = addr & 0xffff;
+
+	if (enet_write_reg(dev, 0, 0x4, 1, &write_val)) {
+		printf("Failed to write low addr\n");
+		return 1;
+	}
+
+	if (enet_read_reg(dev, 0, 0x8, 1, &low)) {
+		printf("Failed to read low addr\n");
+		return 1;
+	}
+
+	write_val = addr + 2;
+
+	if (enet_write_reg(dev, 0, 0x4, 1, &write_val)) {
+		printf("Failed to write high addr\n");
+		return 1;
+	}
+
+	if (enet_read_reg(dev, 0, 0x8, 1, &high)) {
+		printf("Failed to read high addr\n");
+		return 1;
+	}
+
+	*(val) = ((uint32_t)(high << 16) | low);
+
+	return 0;
+}
+
+static int write_indirect(struct enet_dev *dev, uint32_t addr, uint32_t val)
+{
+	uint32_t write_val;
+
+	write_val = ((uint32_t)((val & 0xffff) << 16) | (addr & 0xffff));
+
+	if (enet_write_reg(dev, 0, 0x0, 1, &write_val)) {
+		printf("Failed to write low addr and val\n");
+		return 1;
+	}
+
+	write_val = ((uint32_t)(val & 0xffff0000) | (addr + 2));
+
+	if (enet_write_reg(dev, 0, 0x0, 1, &write_val)) {
+		printf("Failed to write high addr and val\n");
+		return 1;
+	}
+
+	return 0;
+}
 
 int main(int argc, char **argv)
 {
 	struct enet_dev *dev;
-	clock_t start, end;
-	double ttime;
-	char *ma;
-	int i;
+	uint32_t addr = 0xc0;
+	uint32_t write_val = 0x12345678;
+	uint32_t reset_val = 0x8888aaaa;
+	uint32_t val;
 
-	puts("Start testing libenet...");
 	dev = enet_open_dev(atoi(argv[1]), atoi(argv[2]),
 			    atoi(argv[3]), atoi(argv[4]));
 	if (!dev) {
+		printf("Failed to open device");
 		return 1;
 	}
 
-	ma = memalign(4 * 1024, 8 * 1024);
-	if (!ma) {
-		puts("Error: Failed to allocate memory aligned\n");
+	if (read_indirect(dev, addr, &val)) {
+		printf("Failed to read indirect\n");
+		return 1;
+	}
+
+	printf("Before write val=%#x\n", val);
+
+	if (write_indirect(dev, addr, write_val)) {
+		printf("Failed to write indirect\n");
+		return 1;
+	}
+
+	if (read_indirect(dev, addr, &val)) {
+		printf("Failed to read indirect\n");
+		return 1;
+	}
+
+	printf("After write val=%#x\n", val);
+
+	if (write_indirect(dev, addr, reset_val)) {
+		printf("Failed to write indirect\n");
+		return 1;
+	}
+
+	if (dev)
 		enet_close_dev(dev);
-		return 1;
-	}
-
-	start = clock();
-	for (i = 0; i < RUNS; i++) {
-		enet_read_reg(dev, 0, 0, TRANSFER_SIZE / 4, (uint32_t *)ma);
-	}
-	end = clock();
-
-	ttime = ((double)(end - start) / CLOCKS_PER_SEC);
-
-	printf("Total time %f seconds to execute\n", ttime);
-	printf("Read bandwidth %.2f Mbyte/sec\n",
-	       RUNS * (double) (TRANSFER_SIZE / 4) / ttime / 1000000.);
-
-
-	start = clock();
-	for (i = 0; i < RUNS; i++) {
-		enet_write_reg(dev, 0, 0, TRANSFER_SIZE / 4, (uint32_t *)ma);
-	}
-	end = clock();
-
-	ttime = ((double)(end - start) / CLOCKS_PER_SEC);
-
-	printf("Total time %f seconds to execute\n", ttime);
-	printf("Write bandwidth %.2f Mbyte/sec\n",
-	       RUNS * (double) (TRANSFER_SIZE / 4) / ttime / 1000000.);
-
-
-	free(ma);
-	enet_close_dev(dev);
-	puts("End testing libenet...");
 	return 0;
 }
