b84292aee68673c675927811c0aaabd21a37d8ec
Kamal Heib <kamalheib1@gmail.com>
2018-08-13 13:09:59 +0300

    libenet: dump spi registers
    Signed-off-by: Kamal Heib <kamalheib1@gmail.com>

--- test/test.c
+++ test/test.c
@@ -23,69 +23,12 @@
 #include <time.h>
 #include "enet.h"
 
-static int read_indirect(struct enet_dev *dev, uint32_t addr, uint32_t *val)
-{
-	uint32_t write_val;
-	uint32_t high;
-	uint32_t low;
-
-	write_val = addr & 0xffff;
-
-	if (enet_write_reg(dev, 0, 0x4, 1, &write_val)) {
-		printf("Failed to write low addr\n");
-		return 1;
-	}
-
-	if (enet_read_reg(dev, 0, 0x8, 1, &low)) {
-		printf("Failed to read low addr\n");
-		return 1;
-	}
-
-	write_val = addr + 2;
-
-	if (enet_write_reg(dev, 0, 0x4, 1, &write_val)) {
-		printf("Failed to write high addr\n");
-		return 1;
-	}
-
-	if (enet_read_reg(dev, 0, 0x8, 1, &high)) {
-		printf("Failed to read high addr\n");
-		return 1;
-	}
-
-	*(val) = ((uint32_t)(high << 16) | low);
-
-	return 0;
-}
-
-static int write_indirect(struct enet_dev *dev, uint32_t addr, uint32_t val)
-{
-	uint32_t write_val;
-
-	write_val = ((uint32_t)((val & 0xffff) << 16) | (addr & 0xffff));
-
-	if (enet_write_reg(dev, 0, 0x0, 1, &write_val)) {
-		printf("Failed to write low addr and val\n");
-		return 1;
-	}
-
-	write_val = ((uint32_t)(val & 0xffff0000) | (addr + 2));
-
-	if (enet_write_reg(dev, 0, 0x0, 1, &write_val)) {
-		printf("Failed to write high addr and val\n");
-		return 1;
-	}
-
-	return 0;
-}
-
 int main(int argc, char **argv)
 {
 	struct enet_dev *dev;
-	uint32_t addr = 0x4a0;
-	uint32_t write_val = 0x12345678;
 	uint32_t val;
 	int rc = 0;
+	int i;
 
 	dev = enet_open_dev(atoi(argv[1]), atoi(argv[2]),
 			    atoi(argv[3]), atoi(argv[4]));
@@ -94,19 +37,14 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	if (write_indirect(dev, addr, write_val)) {
-		rc = 1;
-		printf("Failed to write indirect\n");
-		goto out;
+	for (i = 0xb000; i < 0xbfff; i++) {
+		rc = enet_read_reg(dev, 0, i, 1, &val);
+		if (rc) {
+			printf("Failed to read from address %#x\n", i);
+			goto out;
+		}
+		printf("val=%#x\n", val);
 	}
-
-	if (read_indirect(dev, addr, &val)) {
-		rc = 1;
-		printf("Failed to read indirect\n");
-		goto out;
-	}
-
-	printf("val=%#x\n", val);
 
 out:
 	if (dev)
